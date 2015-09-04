0a78841f100f74deb6fc887a5ea13ffde3bcdcf6
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-09-04 09:50:12 +0300

    refactor configuration

--- Panteon.Sdk/Configuration/TaskConfigProviderBase.cs
+++ Panteon.Sdk/Configuration/TaskConfigProviderBase.cs
@@ -1,16 +1,40 @@
-﻿using Autofac.Extras.NLog;
+﻿using System.IO;
+using System.Reflection;
+using Autofac.Extras.NLog;
+using Panteon.Sdk.IO;
+using Panteon.Sdk.Utils;
 
 namespace Panteon.Sdk.Configuration
 {
     public abstract class TaskConfigProviderBase<TSettings>
     {
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly IFileReader _fileReader;
         public ILogger Logger { get; set; }
 
-        protected TaskConfigProviderBase(ILogger logger)
+        protected TaskConfigProviderBase(ILogger logger, IJsonSerializer jsonSerializer, IFileReader fileReader)
         {
+            _jsonSerializer = jsonSerializer;
+            _fileReader = fileReader;
             Logger = logger;
         }
 
-        public abstract TSettings ParseSettings(string configFilePath = null);
+        public virtual TSettings ParseSettings(string configFilePath = null)
+        {
+            string asmLocation = Assembly.GetExecutingAssembly().Location;
+
+            string directoryName = Path.GetDirectoryName(asmLocation);
+
+            if (!string.IsNullOrEmpty(directoryName))
+            {
+                configFilePath = configFilePath ?? Path.Combine(directoryName, "config.json");
+            }
+
+            FileContentResult result = _fileReader.ReadFileContent(configFilePath);
+
+            TSettings settings = _jsonSerializer.Deserialize<TSettings>(result.Content);
+
+            return settings;
+        }
     }
 }
--- Panteon.Sdk/WorkerModule.cs
+++ Panteon.Sdk/WorkerModule.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using Autofac.Extras.NLog;
 using NLog;
+using Panteon.Sdk.IO;
 using ILogger = Autofac.Extras.NLog.ILogger;
 
 namespace Panteon.Sdk
@@ -10,6 +11,9 @@ namespace Panteon.Sdk
         protected override void Load(ContainerBuilder builder)
         {
             builder.Register(c => new LoggerAdapter(LogManager.GetCurrentClassLogger())).As<ILogger>().SingleInstance();
+
+            builder.RegisterType<FileSystem>().As<IFileSystem>().SingleInstance();
+            builder.RegisterType<FileReader>().As<IFileReader>().SingleInstance();
 
             base.Load(builder);
         }
